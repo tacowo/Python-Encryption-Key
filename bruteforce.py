d39e2b65a1df42194d601afdca50139a5744ba51
tacoooジ <95009317+tacowo@users.noreply.github.com>
2023-11-14 09:55:40 -0600

    add files via upload
    fixed error, now only counts back 5 to make it easier to prove it is solvable

--- bruteforce.py
+++ bruteforce.py
@@ -8,7 +8,9 @@ guess = ""
 while True:
     if passcheck(guess) == True:
         break
-    guess = random.randrange(10000000000001, 16119199999999)
+    guess = random.randrange(16119192000001, 16119192315199)
     count = count + 1      # 16119192315184
     print("ATTEMPT " + str(count) + " UNSUCCESSFUL... REROLL: " + str(guess) + " " + str(return_to_text(guess))) 
-    time.sleep(0.5)
+    time.sleep(0)
+print("SUCCESSFUL PASSKEY, LOGGED: " + str(guess))
+print("DECODED: " + return_to_text(guess))
